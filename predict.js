843b25357983ff2323a9abf54d3b019111368e6e
Coreus <706874+Coreusa@users.noreply.github.com>
2021-04-23 13:27:31 +0200

    Renaming readmes

--- predict.js
+++ predict.js
@@ -3,20 +3,17 @@ var queryString = require('querystring');
 
 // Analyze a string utterance.
 getPrediction = async () => {
-    // Values to modify.
     // YOUR-APP-ID: The App ID GUID found on the www.luis.ai Application Settings page.
-    const LUIS_appId = "LUIS_APP_ID";
+    const LUIS_appId = "1ef1c20b-882b-49a5-ad82-6cea28912b0d";
 
     // YOUR-PREDICTION-KEY: Your LUIS authoring key, 32 character value.
-    const LUIS_predictionKey = "LUIS_PREDICTION_KEY";
+    const LUIS_predictionKey = "76418d7f42e441e186a7b0f391c2fc35";
 
     // YOUR-PREDICTION-ENDPOINT: Replace this with your authoring key endpoint.
-    const LUIS_endpoint = "LUIS_ENDPOINT_URL";
+    const LUIS_endpoint = "https://westus.api.cognitive.microsoft.com/";
 
     // The utterance you want to use.
     const utterance = process.argv[2]
-    // const utterance = "I want two large pepperoni pizzas on thin crust please";
-    //////////
 
     // Create query string
     const queryParams = {
@@ -33,9 +30,10 @@ getPrediction = async () => {
     const response = await requestPromise(URI);
 
     // Display the response from the REST call.
-    console.log(response);
-    // console.log(process.argv[2]);
+    console.log(JSON.stringify(response, null, 4));
 }
 
 // Pass an utterance to the sample LUIS app
-getPrediction().then(()=>console.log("done")).catch((err)=>console.log(err));
+getPrediction()
+  .then(() => console.log("done"))
+  .catch((err) => console.log(err));
